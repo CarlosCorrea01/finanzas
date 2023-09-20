0b67be622ddc08b5fea69ebd162c25fb6954d6ae
Carlos Fabricio Correa <fabricorrea011015@hotmail.com>
2023-09-20 01:09:39 -0300

    Probrando checkbox

--- js/main.js
+++ js/main.js
@@ -1,59 +1,72 @@
-
-//funcion para caclular el interes
-function interesCompuesto(capital,interes,periodo,aporte){
-    let r = (interes/100)/12
-    let t = periodo*12
+let calculo = (cap,tasa,plazo,aporte) =>{
+    let r = (tasa/100)/12
+    let t = plazo*12
     let x = (1+r)
-    let primera = Math.pow(x,t)
-    let segunda = (1+r)
-    let capitalFinal = capital*primera + aporte*((primera-1)/(segunda-1))
-
-    return capitalFinal
+    let x1 =Math.pow(x,t)
+    let balance = (cap*x1 + aporte*((x1-1)/(x-1))).toFixed(0)
+    return balance;
 }
-//constructora de objetos (inversiones)
+
+//Constructor de objetos Inversiones
 class Inversiones {
     constructor(info){
         this.plazo = info.plazo;
         this.capital = info.capital;
         this.interes = info.interes;
-        this.aportes = info.aportes;
-        
+        this.aportes = info.aportes;  
     }
-
 }
 //array donde se almacenan los objetos (inversiones)
 const inversiones = []
+//M
+function calcular(){
+    let cap = document.getElementById("capIni").value;
+    let tasa = document.getElementById("tasa").value;
+    let plazo = document.getElementById("plazo").value;
+    let aporte = document.getElementById("aportes").value; 
+    
+    let balFinal = calculo(cap,tasa,plazo,aporte)
+    let bal = document.getElementById("balance")
+    bal.innerText = `$${balFinal}`
+    let tApor = document.getElementById(`aportesTotales`)
+    tApor.innerText = `$${parseFloat(cap)+parseFloat(plazo*12*aporte)}`
+    let tInteres = document.getElementById(`interesTotal`)
+    tInteres.innerText =`$${balFinal-(parseFloat(cap)+parseFloat(plazo*12*aporte))}`
+}
 
-//funcion agregar y calcular interes
-function agregarCalcular(){
-    //capturar valores
-    let capitalInicial = document.getElementById("capitalInicial").value;
-    let tasaInteres = document.getElementById("rentabilidad").value;
-    let periodoAhorro = document.getElementById("plazo").value;
-    let aportesMesuales = document.getElementById("aportes").value; 
-    //agegar al array la nueva inversion
+function guardar(){
+    let cap = document.getElementById("capIni").value;
+    let tasa = document.getElementById("tasa").value;
+    let plazo = document.getElementById("plazo").value;
+    let aporte = document.getElementById("aportes").value; 
     inversiones.push(new Inversiones({
-        plazo: periodoAhorro,
-        capital: capitalInicial,
-        interes: tasaInteres,
-        aportes: aportesMesuales,
+        plazo: plazo,
+        capital: cap,
+        interes: tasa,
+        aportes: aporte,
     }))
     //Agregar al local
-    sessionStorage.setItem("misinversiones",JSON.stringify(inversiones))
-    //
-    let calculo = interesCompuesto(capitalInicial,tasaInteres,periodoAhorro,aportesMesuales).toFixed(2)
-
-    let caja = document.createElement("div")
-    caja.className = "cajaInver"
-    caja.innerHTML =`
-        <p>CAPITAL:$${capitalInicial}; PLAZO: ${periodoAhorro} AÑOS; INTERES: ${tasaInteres}%; APORTES: $${aportesMesuales}</p>
-        <h4>CAPITAL FINAL $${calculo}</h4>
-        `
-        document.body.append(caja)
+    localStorage.setItem("misinversiones",JSON.stringify(inversiones))
 }
 
-//Ejecutar la funcion cuando se preciona el boton
-document.getElementById("agregar").onclick = function (){
-    agregarCalcular();
+
+
+
+document.getElementById("calcular").onclick = ()=>{
+    let check = document.getElementById(`ch1`).checked;
+    switch (check) {
+        case true :
+            guardar();
+            calcular();
+            break;
     
+        default:
+            calcular();
+            break;
+    }
 }
+
+document.getElementById("reiniciar").onclick = ()=>{
+location.reload();
+}
+
