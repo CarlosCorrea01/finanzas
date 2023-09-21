bb6d004713ca880e6dace84445522710f1e72102
Carlos Fabricio Correa <fabricorrea011015@hotmail.com>
2023-09-21 02:55:37 -0300

    ultimos detalles

--- js/main.js
+++ js/main.js
@@ -46,27 +46,125 @@ function guardar(){
         aportes: aporte,
     }))
     //Agregar al local
-    localStorage.setItem("misinversiones",JSON.stringify(inversiones))
+    sessionStorage.setItem("misinversiones",JSON.stringify(inversiones))
 }
 
-
+//ver inversiones cargadas
+let misGuardados = document.getElementById("misGuardados")
+const renderisado = (inversiones)=>{
+    misGuardados.innerHTML = ""
+    inversiones.forEach(item => {
+        let div = document.createElement(`div`);
+        div.className = "d-flex justify-content-center m-2 border border-success border-2"
+        div.innerHTML = `
+        <p class="m-3 fs-6 fw-bold">CAPITAL:$${item.capital}</p>
+        <p class="m-3 fs-6 fw-bold">PLAZO:${item.plazo} AÑOS</p>
+        <p class="m-3 fs-6 fw-bold">RENDIMIENTO:${item.interes}</p>
+        <p class="m-3 fs-6 fw-bold">APORTES:$${item.aportes} MENSUALES</p>
+        <p class="m-3 fs-6 fw-bold">BALANCE FINAL:$${calculo(item.capital,item.interes,item.plazo,item.aportes)}</p>
+        `;
+        misGuardados.append(div);
+    });
+}
 
 
 document.getElementById("calcular").onclick = ()=>{
-    let check = document.getElementById(`ch1`).checked;
-    switch (check) {
-        case true :
+    Swal.fire({
+        icon: 'question',
+        title: 'Deseas guardar los datos?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        denyButtonText: `Solo calcular`,
+      }).then((result) => {
+        if (result.isConfirmed) {
             guardar();
             calcular();
-            break;
-    
-        default:
+            renderisado(inversiones);
+          Swal.fire('Guardado', '', 'success')
+        } else if (result.isDenied) {
             calcular();
-            break;
-    }
+          Swal.fire('Calculo realizado', '', 'info')
+        }
+      })
 }
-
 document.getElementById("reiniciar").onclick = ()=>{
-location.reload();
+    location.reload();
+    sessionStorage.clear();
 }
+/*
+let datadolar = document.getElementById("dolar");
 
+fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => 
+    data.forEach(key =>{
+        let div = document.createElement(`div`);
+        div.className = "d-flex justify-content-center m-2 border border-success border-2"
+        div.innerHTML = `
+        <div>
+        <p class=" fs-6 fw-bold">DOLAR:${key.nombre}</p>
+        <p class="fs-6 fw-bold">COMPRA:${key.compra}</p>
+        <p class=" fs-6 fw-bold">VENTA:${key.venta}</p>
+        </div>
+        `;
+        datadolar.append(div)
+    })) ;
+
+*/
+
+let dolarblue = document.getElementById("dolarblue");
+fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => 
+    dolarblue.innerHTML = `
+        <h2 class="">Dolar ${data[1].nombre}</h2>
+        <div class="d-flex justify-content-center w-100">
+            <p class="m-2 fs-3 fw-bold text-success">C: $${data[1].compra}</p>
+            <p class="m-2 fs-3 fw-bold text-success">V: $${data[1].venta}</p>
+        </div>
+    `
+);
+
+let otros = document.getElementById("otrosdolar");
+fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => {
+    let exclu = data[1];
+    data.forEach(item=>{
+        if(item!==exclu){
+            let div = document.createElement(`div`);
+            div.className = "col m-1"
+            div.innerHTML = `
+            <div class=" d-flex justify-content-around align-items-center border border-success border-2">
+            <p class="fs-4">${item.nombre}</p>
+            <div class="d-flex justify-content-center">
+              <p>compra</p>
+              <p class="ms-2 fs-4">$${item.compra}</p>
+            </div>
+            <div  class="d-flex justify-content-center ">
+              <p>venta</p>
+              <p class="ms-2 fs-4">$${item.venta}</p>
+            </div>
+            </div>
+        `;
+        otros.append(div);
+        }
+    })
+  }
+    
+);
+  
+/*<p class="fs-4">Dolar oficial</p>
+              <div class="d-flex justify-content-center">
+                <p>compra</p>
+                <p class="ms-2 fs-4">$55</p>
+              </div>
+              <div  class="d-flex justify-content-center ">
+                <p>venta</p>
+                <p class="ms-2 fs-4">$55</p>
+              </div>
+
+<div class="bg-primary d-flex justify-content-around align-items-center">
+              
+</div>*/
